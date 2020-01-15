45c5203a971b3ce697236e2d2eaf2b002657ccc2
imeoer <imeoer@gmail.com>
2020-01-15 17:18:37 +0800

    remove app.html

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,14 +5,15 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: process.env.npm_package_name || '',
+    title: 'Occlum - A library OS empowering everyone to run every application in secure enclaves',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      { hid: 'description', name: 'description', content: 'A library OS empowering everyone to run every application in secure enclaves' },
+      { name: 'keywords', content: 'Occlum,LibOS,Secure,Enclave,Secure Enclave,SGX,Rust' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'shortcut icon', type: 'image/png', href: '/favicon.png' }
     ]
   },
   /*
