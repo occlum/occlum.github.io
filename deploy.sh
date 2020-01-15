dae544e11e7c5927fbd4b810e823b5001184324b
imeoer <imeoer@gmail.com>
2020-01-15 15:47:48 +0800

    update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
+# please excute on gh-pages branch
+
 npm run generate
-git add dist
-git commit -m "build"
-git subtree pull --prefix dist origin master
-git subtree push --prefix dist origin master
+git checkout master
