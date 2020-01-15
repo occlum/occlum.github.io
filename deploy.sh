fab4ec3d9d2ce823eaa7ceb9615b4ecde027dd62
imeoer <imeoer@gmail.com>
2020-01-15 15:54:21 +0800

    update deploy script

--- deploy.sh
+++ deploy.sh
@@ -2,10 +2,10 @@
 
 # please excute on gh-pages branch
 
-temp=`mktemp`
-npm run generate
-cp dist/* $temp/
-git checkout master
-cp $temp/* ./
-git add .
+temp=`mktemp` && \
+npm run generate && \
+cp dist/* $temp/ && \
+git checkout master && \
+cp $temp/* ./ && \
+git add . && \
 git commit -m "build"
