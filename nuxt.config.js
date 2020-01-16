dbf9e465cdbb3a22927aa0bc50d1c28201c710ed
imeoer <imeoer@gmail.com>
2020-01-16 11:29:44 +0800

    improve keywords meta

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,7 +10,7 @@ module.exports = {
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: 'A library OS empowering everyone to run every application in secure enclaves' },
-      { name: 'keywords', content: 'Occlum,LibOS,Secure,Enclave,Secure Enclave,SGX,Rust' }
+      { name: 'keywords', content: 'Occlum,LibOS,Library OS,Secure,Enclave,Secure Enclave,SGX,Rust,Cloud' }
     ],
     link: [
       { rel: 'shortcut icon', type: 'image/png', href: '/favicon.png' }
