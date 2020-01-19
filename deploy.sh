9718d44124191cfe18ce403520f0624cf7b4108e
imeoer <imeoer@gmail.com>
2020-01-19 10:29:10 +0800

    improve deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
-# please excute on gh-pages branch
-
 git checkout gh-pages && \
 temp=`mktemp -d` && \
 npm run generate && \
 cp -R dist/* $temp/ && \
 git checkout master && \
+rm -rf ./dist ./font ./static && \
 cp -R $temp/* ./ && \
 git add . && \
-git commit -m "build"
+git commit -m "build" && \
 git push origin master && \
 git checkout gh-pages
